20be6e5452cdc5e824690e4f7acf2d52813068b3
Oleg <kabachiyoleg@gmail.com>
2021-02-02 14:41:41 +0100

    auth server

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Admin:7fLIBORZdUygnGWW@cluster0.im6da.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Admin:7fLIBORZdUygnGWW@cluster0.im6da.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,67 @@
-module.exports.login = (req,res) =>{
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
+
+
+module.exports.login = async (req,res) =>{
+  //email password
+   const candidate = await User.findOne({
+    email: req.body.email
+   })
+    if(candidate){
+        // check pass
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if(passwordResult){
+            //gen token - pass correct
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60*60})
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        }else{
+            // pass is not correct
+            res.status(401).json({
+                message: 'password incorrect, try again'
+            })
         }
-    })
+    }else{
+        // user not found
+        res.status(404).json({
+            message: 'User email not found'
+        })
+    }
 }
 
 
-module.exports.register = (req,res)=> {
-    res.status(200).json({
-        register: 'from controller'
-    })
-}
+module.exports.register = async (req,res)=> {
+   //email password
+   const candidate = await User.findOne({email: req.body.email})
+
+    if(candidate){
+        //user avalible - error
+        res.status(409).json({
+            message: 'Email is occupied'
+        })
+    }else{
+        //create new user
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try{
+            await user.save()
+            res.status(201).json(user)
+        }catch(error){
+            // error handling
+            errorHandler(res, error)
+        }
+
+    }
+  }
